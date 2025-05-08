1984244ad77e1cd8c58d30bb3cdf1d3ca361f054
isaaczanafria <isaaczanafria@gmail.com>
2025-05-08 17:25:36 -0500

    cambios de diseño profile, funcionamiento de actualizar perfil y contraseña

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router'
 import Login from './pages/Login'
 import AuthLayout from './components/layouts/AuthLayout'
+import Profile from './pages/Profile'
 
 
 export default function App() {
@@ -9,7 +10,7 @@ export default function App() {
     
       <Routes>
         <Route element={<AuthLayout/>}>
-        <Route path='/' element={<h1>Profile</h1>}/>
+        <Route path='/' element={<Profile/>}/>
         <Route path='/servicio_estudiante' element={<h1>Servicio de estudiante</h1>}/>
         <Route path='/servicio_admin' element={<h1>Servicio de Admin</h1>}/>
         <Route path='/lista_estudiantes' element={<h1>Lista de estudiantes para admin</h1>}/>
---
+++ src/axios/users/user.js
@@ -0,0 +1,11 @@
+import { instance } from "../instance"
+
+export async function update(request, id) {
+    try {
+        const { status } = await instance.put(`/users/${id}`, request)
+        return status
+    } catch (error) {
+        throw error
+    }
+}
+
