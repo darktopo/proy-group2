9853a24759b36045c6511a8975bc710ccee49e49
falconblade <falconbladex1@gmail.com>
2025-05-09 12:32:23 -0500

    arreglos de archivo duplicado , search bar y appbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,25 @@
-<<<<<<< HEAD
-import { Route, Routes } from "react-router";
-import Login from "./pages/Login";
-import AuthLayout from "./components/layouts/AuthLayout";
-import HorasServicioAdmin from "./pages/HorasServicioAdmin";
-=======
 import { Route, Routes } from 'react-router'
 import Login from './pages/Login'
 import AuthLayout from './components/layouts/AuthLayout'
 
-import HorasServicioAdminPage from './pages/HorasServicioAdminPage'
+import HorasServicioAdmin from './pages/HorasServicioAdmin'
 import HoraServEstu from './pages/HoraServEstu'
 import ForbiddenPage from "./pages/Forbidden";
 
 
 import Profile from './pages/Profile'
 
->>>>>>> 631d8d07baec887ff1d1089973ced766e722d262
 
 export default function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Routes>
-        <Route element={<AuthLayout />}>
-          <Route path="/" element={<h1>Profile</h1>} />
-          <Route
-            path="/servicio_estudiante"
-            element={<h1>Servicio de estudiante</h1>}
-          />
-          <Route path="/servicio_admin" element={<HorasServicioAdmin/>} />
-          <Route
-            path="/lista_estudiantes"
-            element={<h1>Lista de estudiantes para admin</h1>}
-          />
-        </Route>
-
-        <Route path="/login" element={<Login />} />
-=======
 
       <Routes>
 
         <Route element={<AuthLayout />}>
           <Route path='/' element={<Profile />} />
           <Route path='/servicio_estudiante' element={<HoraServEstu />} />
-          <Route path='/servicio_admin' element={<HorasServicioAdminPage />} />
+          <Route path='/servicio_admin' element={<HorasServicioAdmin />} />
           <Route path='/lista_estudiantes' element={<h1>Lista de estudiantes para admin</h1>} />
 
 
@@ -51,7 +27,6 @@ export default function App() {
 
         <Route path='/login' element={<Login />} />
         <Route path="/forbiden" element={<ForbiddenPage />} />
->>>>>>> 631d8d07baec887ff1d1089973ced766e722d262
       </Routes>
     </>
   );
